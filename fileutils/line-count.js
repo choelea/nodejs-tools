69a820bb80de34b62ab3c5fdaa3e05cde857e9b8
Joe Li <joe.li@okchem.com>
2017-05-03 08:06:29 +0800

    Initiate Project

---
+++ fileutils/line-count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/env node
+var argv = require('yargs')
+    .usage('Usage: $0 <command> [options]')
+    .command('count', 'Count the lines in a file')
+    .example('$0 count -f foo.js', 'count the lines in the given file')
+    .alias('f', 'file')
+    .nargs('f', 1)
+    .describe('f', 'Load a file')
+    .demandOption(['f'])
+    .help('h')
+    .alias('h', 'help')
+    .epilog('copyright 2015')
+    .argv;
+ 
+var fs = require('fs');
+var s = fs.createReadStream(argv.file);
+ 
+var lines = 0;
+s.on('data', function (buf) {
+    lines += buf.toString().match(/\n/g).length;
+});
+ 
+s.on('end', function () {
+    console.log(lines);
+});
